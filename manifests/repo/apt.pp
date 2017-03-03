3ed8ce17c5b9cae3fb3b46b732a9162bf5b69f45
Ilya Fourmanov <ilya.fourmanov@gmail.com>
2017-03-03 17:39:25 +0000

    Elastic beat repo update

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -2,11 +2,11 @@
 class beats::repo::apt() {
   apt::source { 'elastic-beats':
     comment  => 'Official Elastic beats repositories',
-    location => 'https://packages.elastic.co/beats/apt',
+    location => 'https://artifacts.elastic.co/packages/5.x/apt',
     release  => 'stable',
     repos    => 'main',
     key      => {
-      id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+      id     => '0xD27D666CD88E42B4',
       server => 'pgp.mit.edu'
     },
     include  => {
