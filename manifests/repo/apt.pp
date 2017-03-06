a28f824f7b66af9f9353aa7677ee2fbf86ee717b
Ilya Fourmanov <ilya.fourmanov@gmail.com>
2017-03-06 10:36:20 +0000

    Full apt key for elastic.co

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -6,7 +6,7 @@ class beats::repo::apt() {
     release  => 'stable',
     repos    => 'main',
     key      => {
-      id     => 'D88E42B4',
+      id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
       server => 'pgp.mit.edu'
     },
     include  => {
